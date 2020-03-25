202b00feb79ab57b870eb4bd2c2e01e00c8c9a19
Olaf <olaf.ruvag@gmail.com>
2020-03-24 20:59:11 -0600

    Aplicacion ya tiene mapa y paginas principales

---
+++ src/app/google-map/google-map.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-google-map',
+  templateUrl: './google-map.component.html',
+  styleUrls: ['./google-map.component.scss'],
+})
+export class GoogleMapComponent{
+
+  constructor() { }
+
+
+}
--- src/app/map/map.page.ts
+++ src/app/map/map.page.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-import { NavController } from '@ionic/angular';
+import { Component, OnInit, ViewChild, ElementRef, AfterContentInit} from '@angular/core';
 import { CompileShallowModuleMetadata } from '@angular/compiler';
+import { GoogleMapComponent } from '../google-map/google-map.component';
+declare var google;
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.page.html',
   styleUrls: ['./map.page.scss'],
 })
-export class MapPage implements OnInit {
 
-  constructor(public navCtrl: NavController) { }
+export class MapPage implements OnInit, AfterContentInit {
+  map:any;
+  @ViewChild('map',{ static: true })mapElement: ElementRef;
+
+  constructor() { 
+    
+  }
 
   ngOnInit() {
   }
@@ -17,5 +23,24 @@ export class MapPage implements OnInit {
   funcionMapa(){
     console.log("Aqui es donde va funcion para latitud y longitud ponerlas en el mapa")
   }
+
+  ngAfterContentInit():void{
+    this.map =new google.maps.Map(this.mapElement.nativeElement,{
+      zoom: 14,
+      center: {lat:20.6736, lng:-103.344},
+    });
+  }
+
+  /*loadMap(){
+    var map=new google.maps.Map(this.mapElement.nativeElement,{
+      zoom: 14,
+      center: {lat:20.6736, lng:-103.344},
+    });
+    return map;
+  }
+
+  IonViewDidLoad() {
+    this.map=this.loadMap();
+  }*/
 
 }
